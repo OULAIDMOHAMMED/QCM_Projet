75dc41a8351d3650a7eeca069a31507f865dae8a
Oulaid Mohammed <mohamedoulaid93@gmail.com>
2025-06-13 13:47:34 +0100

    qst

---
+++ exambackend/exambackend/Models/Question.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace exambackend.Models
+{
+    public class Question
+    {
+        [Key]
+        public int Id { get; set; }
+        public required string QuestionText { get; set; }
+        public List<string> Answers { get; set; } = new List<string>();
+        public List<int> CorrectIndexes { get; set; } = new List<int>(); // Indices des réponses correctes
+    }
+
+}
